511ea7a988158a7bf40c54d0f365b960242074bd
Madina <Madina@10.0.0.80>
2021-02-26 20:46:56 -0500

    Adding some file

---
+++ First project 1/src/assinments/AssignmentArrays3.java
@@ -0,0 +1,24 @@
+package assinments;
+
+
+
+public class AssignmentArrays3 {
+	
+	public static void main(String[] args)   {
+		String str =  "kevin";
+		String str1 =  "kemall";
+		String str2 =  "linda";
+		String str3 =  "russel";
+		
+		
+		
+		String lastWord = str3.substring(str3.lastIndexOf(" ")+1);
+		System.out.print(lastWord);
+	   }
+	
+
+}
+	
+	
+	
+	
