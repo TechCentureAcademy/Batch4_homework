d3b695b4ffab85f6fc3c055edf3a3a4b9a2e94f3
nigor <nigor@DESKTOP-1BI09O3.attlocal.net>
2021-03-03 23:11:32 -0500

    Adding first file

---
+++ Batch4homeworks/HomeworkFebruaryFive.java
@@ -0,0 +1,32 @@
+package javaHomework;
+
+import java.util.Scanner;
+
+public class HomeworkFebruaryFive {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.println("Please enter number");
+		int num = scanner. nextInt();
+		if (num >= 0) {
+			System.out.println("Your number is positive");
+		}else {
+				System.out.println("Your number is negative");
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
